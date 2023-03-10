0a6ba00d88d49c3bffbbe69243001e32177a03ba
Baia Sergeenko <106155514+bsergeenko@users.noreply.github.com>
2023-03-10 16:14:28 +0100

    Create 10.1 meal.py

---
+++ 10.1 meal.py
@@ -0,0 +1,20 @@
+def main():
+    time = convert(input("what time is it? "))
+    if time >= 7 and time <= 8:
+        print("breakfast time")
+    elif time >= 12 and time <= 13:
+        print("lunch time")
+    elif time >= 18 and time <= 19:
+        print("lunch time")
+    else:
+        print("")
+
+def convert(time):
+    hour, min = time.split(":")
+    hour = float(hour)
+    min = float(min)
+    min = min / 60
+    time = hour + min
+    return time
+
+main()
