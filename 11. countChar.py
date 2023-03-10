7e488ed760671a282cb74499c381d6e30be8f0c0
Baia Sergeenko <106155514+bsergeenko@users.noreply.github.com>
2023-03-10 14:29:39 +0100

    Create 11. countChar.py

---
+++ 11. countChar.py
@@ -0,0 +1,6 @@
+text = input ("please whrite the text: ")
+if text == "":
+    text2 = input ("It is nesessery to whrite the text: ")
+    print (text2, "has", len(text2), "charackter")
+else:
+    print (text, "has", len(text), "charackter")
