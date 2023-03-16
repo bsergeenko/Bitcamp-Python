c4b1990b24ef721ebe617e8be3719f66093adc6c
Baia Sergeenko <106155514+bsergeenko@users.noreply.github.com>
2023-03-16 16:20:41 +0100

    Create 16. plates.py

---
+++ 16. plates.py
@@ -0,0 +1,44 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    while True:
+        # max and min charakter
+        if len(s) < 2 or len(s) > 6:
+            return False
+        
+        # start with two letter
+        if s[0:2].isdigit():
+            return False
+        
+        # numbers in middle
+        for char in s:
+            if char.isdigit():
+                index = s.index(char)
+                if not s[index:].isdigit():
+                    return False
+            
+        # first number cannot be 0
+        i = 0
+        while i < len(s):
+            if s[i].isdigit():
+                if s[i] == "0":
+                    return False 
+                break
+            i += 1
+         
+        
+        # no periods, space...
+        not_allowd = (" ", ".", ",", "!", "?", ";", ":")
+        for char in s:
+            if char in not_allowd:
+                return False
+            
+        return True
+
+main()
