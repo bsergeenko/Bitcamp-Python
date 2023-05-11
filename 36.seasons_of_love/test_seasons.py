dc3bf93e50803384c6c587053dad3ee2a284dd1e
Baia Sergeenko <106155514+bsergeenko@users.noreply.github.com>
2023-05-11 10:08:50 +0200

    Update test_seasons.py

--- 36.seasons_of_love/test_seasons.py
+++ 36.seasons_of_love/test_seasons.py
@@ -1,10 +1,14 @@
-from seasons import minutes, minutes_to_words
+from seasons import minutes, minutes_to_words, valid_format
 
-def test_calculate_birthday():
-    assert minutes("1995-01-01") == 14889600
-    assert minutes("2023-04-15") == 12960
-    assert minutes("2023-04-23") == 1440
+def test_minutes():
+    assert minutes("1995-01-01") == 14895360
+    assert minutes("2023-04-23") == 7200
 
 def test_minutes_to_word():
-    assert minutes_to_words(1) == "one"
-    assert minutes_to_words(158123456) == "one hundred fifty-eight million, one hundred twenty-three thousand, four hundred fifty-six"
+    assert minutes_to_words(1) == "One minutes"
+    assert minutes_to_words(50) == "Fifty minutes"
+    assert minutes_to_words(120) == "One hundred twenty minutes"
+
+def test_valid_foramt():
+    assert valid_format("2001-06-06") == True
+    assert valid_format("2001/06/06") == False
