cdaf7a5df34cffd910adeab8b46a1ad839763def
Baia Sergeenko <106155514+bsergeenko@users.noreply.github.com>
2023-05-11 10:07:36 +0200

    Update 32.lines.py

--- 32.lines.py
+++ 32.lines.py
@@ -1,21 +1,18 @@
 import sys
 
 count = 0
-try:
-    if len(sys.argv) < 2:
-        sys.exit("Too few command-line arguments")
-    elif len(sys.argv) > 2:
-        sys.exit("Too many command-line arguments")
-    elif sys.argv[1].endswith(".py"):
+if len(sys.argv) < 2:
+    sys.exit("Too few command-line arguments")
+elif len(sys.argv) > 2:
+    sys.exit("Too many command-line arguments")
+elif not sys.argv[1].endswith(".py"):
+    sys.exit("Not a python file")
+else:
+    try:
         with open(sys.argv[1], "r") as file:
-            lines = file.readline()
             for line in file:
-                if line.startswith("#") or line =="\n":
-                    continue
-                else:
+                if not line.lstrip().startswith("#") and not line.isspace():
                     count += 1
             print(count)
-    else:
-        print("Not a python file")
-except FileNotFoundError:
-    print("File does not exist")
+    except FileNotFoundError:
+        sys.exit("File does not exist")
