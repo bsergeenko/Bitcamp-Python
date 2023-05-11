fd8d59c6175c97b0cd4441f0cdb2beda99e80139
Baia Sergeenko <106155514+bsergeenko@users.noreply.github.com>
2023-05-11 10:07:02 +0200

    Update 25.game.py

--- 25.game.py
+++ 25.game.py
@@ -3,28 +3,28 @@ import random
 while True:
     try:
         level = int(input ("Level: "))
-
+        number = random.randrange(1, level)
         if level <= 0:
             pass
     except:
         pass
-
-    number = random.randrange(1, level)
-    break
+    else:
+        break
     
 
 while True:
     try:
         guess = int(input("Guess: "))
-        
+        if guess < 0:
+            continue
+        elif guess > number:
+            print("Too large!")
+        elif guess < number:
+            print("Too small!")
+        elif guess == number:
+            print("Just right!")
+            break
     except:
         pass
-    if guess > number:
-        print("Too large!")
-    elif guess < number:
-        print("Too small!")
-    elif guess == number:
-        print("Just right!")
-        break
 
     
