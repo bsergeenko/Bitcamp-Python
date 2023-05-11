0474834b7c828475e61358545dd3f26683a7a11e
Baia Sergeenko <106155514+bsergeenko@users.noreply.github.com>
2023-05-11 10:08:36 +0200

    Update seasons.py

--- 36.seasons_of_love/seasons.py
+++ 36.seasons_of_love/seasons.py
@@ -1,25 +1,38 @@
-from datetime import date
-import datetime
+from datetime import date, timedelta
 import inflect
+import re
+import sys
 
+p = inflect.engine()
 
 def main():
+    birthday = input("Birth date: ")
     try:
-        birthday = input("Your Birthday: ")
-        print(minutes_to_words(minutes(birthday)), "minutes")
-    except ValueError:
-        print("Invalid date")
+        if valid_format(birthday):
+            mins = minutes(birthday)
+            print(minutes_to_words(mins))
+        else:
+            sys.exit("Invalid format")
+    except:
+        sys.exit("Something went wrong")
 
-def minutes(birthday, today = date.today()):
-    birthday = datetime.datetime.strptime(birthday, "%Y-%m-%d")
-    age_in_minutes = (today - birthday.date()).total_seconds() // 60
-    return int(age_in_minutes)
-
+def minutes(birthday):
+    year, month, day = birthday.split("-")
+    formated_birthday = date( year = int(year), month = int(month), day = int(day))
+    today = date.today()
+    diff = today - formated_birthday
+    minutes = int(diff.total_seconds() // 60)
+    return minutes
+  
 def minutes_to_words(minutes):
-    p = inflect.engine().number_to_words(f"{minutes}", andword="")
-    
-    return p
+    word = p.number_to_words(minutes, andword="")
+    return f"{word.capitalize()} minutes"
 
+def valid_format(birth_date):
+    pattern = r"^[0-9]{4}-[0-9]{2}-[0-9]{2}"
+    if re.match(pattern, birth_date):
+        return True
+    return False
 
 if __name__ == "__main__":
     main()
