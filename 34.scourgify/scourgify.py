e0f5f76d90f48a698e29a6f01e7637c5b249f3d1
Baia Sergeenko <106155514+bsergeenko@users.noreply.github.com>
2023-05-11 10:08:25 +0200

    Update scourgify.py

--- 34.scourgify/scourgify.py
+++ 34.scourgify/scourgify.py
@@ -1,29 +1,25 @@
 import sys
 import csv
 
+if len(sys.argv) < 3:
+    sys.exit("Too few command-line arguments")
+elif len(sys.argv) > 3:
+    sys.exit("Too many command-line arguments")
+elif ".csv" not in sys.argv[1] or ".csv" not in sys.argv[2]:
+    sys.exit("Not a CSV file")
+
+students = []
 try:
-    if len(sys.argv) < 3:
-        sys.exit("Too few command-line arguments")
-    elif len(sys.argv) > 3:
-        sys.exit("Too many command-line arguments")
-    elif sys.argv[1].endswith(".csv") and sys.argv[2].endswith(".csv"):
+    with open(sys.argv[1]) as before:
+        reader = csv.DictReader(before)
+        for row in reader:
+            first, last = row["name"].split(",")
+            students.append({"first": first.strip(), "last": last, "house": row["house"]})
+except:
+    sys.exit(f"Could not read {sys.argv[1]}")
 
-        with open(sys.argv[1]) as before:
-            reader = csv.DictReader(before)
-
-            with open(sys.argv[2], "w", newline='') as after:
-                    writer = csv.DictWriter(after, fieldnames = ['first', 'last', 'house'])
-                    writer.writeheader()
-                    for row in reader:
-                        students = {}
-                        first, last = row["name"].split(",")
-                        house = row["house"]
-                        writer.writerow({"first": first, "last": last, "house": house})
-                
-            
-    else:
-        sys.exit("Not csv file")
-
-
-except FileNotFoundError:
-    print("File does not exist")
+with open(sys.argv[2], "w") as after:
+    writer = csv.DictWriter(after, fieldnames = ['first', 'last', 'house'])
+    writer.writeheader()
+    for row in students:
+        writer.writerow({"first": row["first"], "last": row["last"], "house": row["house"]})
