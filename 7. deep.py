ab520ecb2cb0f29219c7e18dd657c76b654cb6f8
Baia Sergeenko <106155514+bsergeenko@users.noreply.github.com>
2023-05-11 10:09:02 +0200

    Update 7. deep.py

--- 7. deep.py
+++ 7. deep.py
@@ -1,4 +1,4 @@
-answer = input("What is the answer to the great question of life, the universe and everything? ")
+answer = input("What is the answer to the great question of life, the universe and everything? ").lower().strip()
 if answer == "42" or answer == "forty-two" or answer == "forty two":
     print("Yes")
 else:
