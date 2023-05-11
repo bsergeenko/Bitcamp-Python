4e872ac4c0bdcedb2b29808feb31ec234374e819
Baia Sergeenko <106155514+bsergeenko@users.noreply.github.com>
2023-05-11 10:08:03 +0200

    Update pizza.py

--- 33.pizza/pizza.py
+++ 33.pizza/pizza.py
@@ -2,25 +2,21 @@ import sys
 import csv
 from tabulate import tabulate
 
+
+if len(sys.argv) < 2:
+    sys.exit("Too few command-line arguments")
+elif len(sys.argv) > 2:
+    sys.exit("Too many command-line arguments")
+elif ".csv" not in sys.argv[1]:
+    sys.exit("Not a CSV file")
+
+pizzas = []
 try:
-    if len(sys.argv) < 2:
-        sys.exit("Too few command-line arguments")
-    elif len(sys.argv) > 2:
-        sys.exit("Too many command-line arguments")
-    elif sys.argv[1].endswith(".csv"):
-        with open(sys.argv[1], "r") as file:
-            reader = csv.DictReader(file)
-            pizzas = []
-
-            for row in reader:
-                pizzas.append(row)
-
-        table = pizzas[1:]
-        headers = pizzas[0]
-                
-        print(tabulate(table, headers, tablefmt="grid"))
-    else:
-        sys.exit("Not csv file")
+    with open(sys.argv[1], "r") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            pizzas.append(row)
+        print(tabulate(pizzas[1:], headers = pizzas[0], tablefmt="grid"))
 
 except FileNotFoundError:
-    print("File does not exist")
+    sys.exit("File does not exist")
